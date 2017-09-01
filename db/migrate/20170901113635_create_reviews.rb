5b9308dbc601b276663e7442ede14a5799064608
iyke254 <nderiony95@gmail.com>
2017-09-01 14:39:47 +0300

    created reviews

---
+++ db/migrate/20170901113635_create_reviews.rb
@@ -0,0 +1,10 @@
+class CreateReviews < ActiveRecord::Migration[5.1]
+  def change
+    create_table :reviews do |t|
+    	t.column :description, :string
+    	t.column :book_id, :integer
+
+    	t.timestamps
+    end
+  end
+end
