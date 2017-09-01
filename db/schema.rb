c1444b5611f2dbc181ee36b2fcc4fe4eb3135f7d
iyke254 <nderiony95@gmail.com>
2017-09-01 14:42:39 +0300

    created reviews model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170831090750) do
+ActiveRecord::Schema.define(version: 20170901113635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20170831090750) do
     t.text "description"
     t.string "author"
     t.string "picture"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "description"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
