95f9b0d31dbff44727598c24fa2656d2565a6371
iyke254 <nderiony95@gmail.com>
2017-09-01 15:07:13 +0300

    added the form for the reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,20 @@ class ReviewsController < ApplicationController
 		@book = Book.find(params[:book_id])
 		@review = @book.reviews.new
 	end
+
+	 def create
+      @book = List.find(params[:book_id])
+      @review = @book.reviews.new(review_params)
+      if @review.save
+        redirect_to book_path(@review.book)
+      else
+        render :new
+      end
+    end
+
+  private
+    def review_params
+      params.require(:review).permit(:description)
+    end
+
 end		
